8637f43e8b16e759fa5c6583bc45e4af909aaa3e
Bouziane_Haithem <haithem.bouziane@ensia.edu.dz>
2026-05-11 04:26:51 +0100

    deploy with base-href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "765084293" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4200881672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
